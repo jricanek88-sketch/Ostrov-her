45bce90fbc51133d823adf64703d3484ced7e5f9
jricanek88-sketch <j.ricanek88@gmail.com>
2026-07-05 15:01:59 +0300

    V09

--- sw.js
+++ sw.js
@@ -1,36 +1,18 @@
-const CACHE_NAME = 'ostrov-her-v2';
-const ASSETS = [
-  './', './index.html', './manifest.json', './icon-192.png', './icon-512.png',
-  './island-bg.png', './stone.png',
-  './icon-coloring.png', './icon-memory.png', './icon-bubbles.png', './icon-puzzle.png', './icon-paw.png'
-];
-
-self.addEventListener('install', (event) => {
+// Tento service worker sam sebe odregistruje a smaze veskerou ulozenou cache.
+// Resi to opakovany problem, kdy prohlizec servíroval starou verzi appky
+// i po nahrani noveho kodu na GitHub Pages.
+self.addEventListener('install', () => {
   self.skipWaiting();
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)).catch(() => {})
-  );
 });
 
 self.addEventListener('activate', (event) => {
   event.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
-    )
-  );
-  self.clients.claim();
-});
-
-// network-first: vždy zkusí nejdřív stáhnout čerstvou verzi ze sítě,
-// a jen když síť selže (offline), použije to, co má uložené v cache.
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    fetch(event.request)
-      .then((response) => {
-        const copy = response.clone();
-        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
-        return response;
+    caches.keys()
+      .then((keys) => Promise.all(keys.map((k) => caches.delete(k))))
+      .then(() => self.registration.unregister())
+      .then(() => self.clients.matchAll({ type: 'window' }))
+      .then((clients) => {
+        clients.forEach((client) => client.navigate(client.url));
       })
-      .catch(() => caches.match(event.request))
   );
 });
